c826faf4e822d77ee7a1f15353a5991c27023b70
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-28 17:02:21 -0300

    Req-27: Cria fetch para pegar categorias de drinks e bebidas. Adiciona local state para a page food para armazenar as categorias. Cria um comp ButtonList que retorna uma lista de botões.

--- cypress/integration/25-recipes_list_spec.js
+++ cypress/integration/25-recipes_list_spec.js
@@ -73,7 +73,7 @@ describe.only('25 - Implemente os elementos da tela principal de receitas respei
   });
 });
 
-describe('26 - Carregue as 12 primeiras receitas de comidas ou bebidas, uma em cada card', () => {
+describe.only('26 - Carregue as 12 primeiras receitas de comidas ou bebidas, uma em cada card', () => {
   it('Caso as receitas sejam de comida, deve-se carregar as 12 primeiras receitas', () => {
     cy.visit('http://localhost:3000/foods', {
       onBeforeLoad(win) {
@@ -95,7 +95,7 @@ describe('26 - Carregue as 12 primeiras receitas de comidas ou bebidas, uma em c
   });
 });
 
-describe('27 - Implemente os botões de categoria para serem utilizados como filtro', () => {
+describe.only('27 - Implemente os botões de categoria para serem utilizados como filtro', () => {
   it('Caso as receitas sejam de comida, deve-se exibir as 5 primeiras categorias de comida', () => {
     cy.visit('http://localhost:3000/foods', {
       onBeforeLoad(win) {
